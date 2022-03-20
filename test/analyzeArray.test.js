0ff304e69150b267ec0f79a18d10fe1e357cbae6
Jerome Tokumei <jerometokumei@gmail.com>
2022-03-20 12:04:26 +0800

    Added analyzeArray toEqual object test

--- test/analyzeArray.test.js
+++ test/analyzeArray.test.js
@@ -5,4 +5,5 @@ test('Analyze small numbers', () => {
   expect(analyzeArray([1, 2, 3, 4]).min).toBe(1);
   expect(analyzeArray([1, 2, 3, 4]).max).toBe(4);
   expect(analyzeArray([1, 2, 3, 4]).length).toBe(4);
+  expect(analyzeArray([1, 2, 3, 4])).toEqual({ average: 2.5, min: 1, max: 4, length: 4});
 });
